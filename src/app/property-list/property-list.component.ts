d27984b315b18f7463031dc16bea98701ddc458c
pravesh <rajputpravesh99@gmail.com>
2022-08-24 17:04:19 +0530

    24aug

--- src/app/property-list/property-list.component.ts
+++ src/app/property-list/property-list.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array <any>=[
+    {
+    "Id": "1",
+    "Name": "suraj",
+    "Type": "House",
+    "Price":"12000"
+  },
+  {
+    "Id": "2",
+    "Name": "singh",
+    "Type": "House1",
+    "Price":"13000"
+  },
+  {
+    "Id": "3",
+    "Name": "negi",
+    "Type": "House3",
+    "Price":"100"
+  },
+  {
+    "Id": "4",
+    "Name": "bd",
+    "Type": "House23",
+    "Price":"111000"
+  },
+  {
+    "Id": "5",
+    "Name": "yjj",
+    "Type": "House75",
+    "Price":"10000"
+  },
+]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/property/property-card/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -6,13 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-card.component.css']
 })
 export class PropertyCardComponent implements OnInit {
+@Input() property : any
 
-Property: any={
-  "Id": "1",
-  "Name": "suraj",
-  "Type": "House",
-  "Price":"12000"
-}
   constructor() { }
 
   ngOnInit(): void {
